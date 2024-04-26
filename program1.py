ddde103a3af92271b240192cc625ea8aa569db8e
vaishnavi verma <98606255+vaishnavi-643@users.noreply.github.com>
2024-04-26 13:19:38 +0530

    solution 1

--- program1.py
+++ program1.py
@@ -1,13 +1,56 @@
-def smallest_missing_positive_integer(nums: List[int]) -> int:
-    """
-    Implement the function smallest_missing_positive_integer 
-    using the provided smallest_missing_positive_integer function 
-    to find the smallest missing positive integer in the given list.
+def smallest_missing_poistive_integer(A): 
 
-    """
+    m = max(A) 
+    if m < 1:
+
+        # In case all values in our array are negative
+        return 1
+    if len(A) == 1:
+
+        # If it contains only one element
+        return 2 if A[0] == 1 else 1
+    l = [0] * m
+    for i in range(len(A)):
+        if A[i] > 0:
+            if l[A[i] - 1] != 1:
+
+                # Changing the value status at the index of our list
+                l[A[i] - 1] = 1
+    for i in range(len(l)):
+
+        # Encountering first 0, i.e, the element with least value
+        if l[i] == 0:
+            return i + 1
+            # In case all values are filled between 1 and m
+    return i + 2
+
+
+# Driver Code
+if __name__ == '__main__':
+    arr = [0, 10, 2, -10, -20]
+    print(smallest_missing_poistive_integer(arr))
+
+
+
+    
+    """"
+    
+    Implement the function smallest missing poistive integer
+    using the provided smallest missing_poistive integer function to find 
+    the length of the smallest missing poistive integer in thr given list .
+
+    """ 
     pass
 
 
+
+
+
+    
+
+
+
+  
 
 
 
